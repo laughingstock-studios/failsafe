bba8d5d0395a725ad09c60ebcf60565f4df6bc66
Vanessa Hillary <van.hill96@gmail.com>
2020-08-23 15:45:16 -0500

    undid selection?

--- Assets/Scripts/Character/Fs4UserControl.cs
+++ Assets/Scripts/Character/Fs4UserControl.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
+[RequireComponent(typeof (Fs4Character))]
 public class Fs4UserControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Fs4Character m_Character;
+    private bool m_Jump;
+
+
+    private void Awake()
     {
-        
+        m_Character = GetComponent<Fs4Character>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if (!m_Jump)
+        {
+            // Read the jump input in Update so button presses aren't missed.
+            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Read the inputs.
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        m_Character.Move(h, crouch, m_Jump);
+        m_Jump = false;
     }
 }
